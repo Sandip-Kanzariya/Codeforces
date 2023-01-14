6cbfd46d649cd1757f8bdccb25be2b61aa6392c6
Kanzaiya Sandip <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-01-14 15:02:35 +0530

    Update 5_A_Divisibility_Problem.cpp

--- Daily Dose/5_A_Divisibility_Problem.cpp
+++ Daily Dose/5_A_Divisibility_Problem.cpp
@@ -1,28 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// // Written by S@ndip
-// int main()
-// {
-//     int t;
-//     cin >> t;
-//     int a, b, c;
-//     while(t--){
-//         c = 0;
-//         cin >> a >> b;
-//         if(a < b){
-//             c = b - a;
-//         }
-//         else{
-//             while(a % b){
-//                 a+=1;
-//                 ++c;
-//             }
-//         }
-//         cout << c << "\n";
-//     }
-//     return 0;
-// }
 #include <iostream>
 using namespace std;
 
