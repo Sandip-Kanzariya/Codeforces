6b08b0a6941c5604bbf2011dd85341572a6efcb0
Kanzaiya Sandip <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-01-23 07:22:43 +0530

    Add files via upload

---
+++ Daily Dose/225_A_Eleven.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <numeric>
+#include <iomanip>
+
+
+using namespace std;
+const int N = 1e7;
+
+using I = int;
+using SI = short int;
+using LI = long int;
+using LLI = long long int;
+using UI = unsigned int;
+using USI = unsigned short int;
+using ULI = unsigned long int;
+using ULLI = unsigned long long int;
+
+#define vi vector<int>
+#define pi pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+
+#define all(a) a.begin(), a.end() - 1
+#define rall(a) a.rbegin(), a.rend()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define ws1(a) cout << a << " "
+#define wr1(a) cout << a << endl
+#define wr2(a, b) cout << a << " " << b << endl
+#define wr3(a, b, c) cout << a << " " << b << " " << c << endl
+#define wr4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl
+#define ww1(a) cout << a << "\n"
+#define ww2(a, b) cout << a << " " << b << "\n"
+#define ww3(a, b, c) cout << a << " " << b << " " << c << "\n"
+#define ww4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << "\n"
+#define FOR(i, s, e) for (int i = s; i < e; ++i)
+#define FORR(i, s, e) for (int i = s; i >= e; --i)
+#define FT first
+#define SC second
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define PRE cout << fixed << setprecision(10);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+vector<pair<int, int>> sides = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+vector<pair<int, int>> movements = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+clock_t startTime;
+void getCurrentTime() { cerr << (double)(clock() - startTime) / CLOCKS_PER_SEC; }
+
+int sum() { return 0; }
+template <typename T, typename... Args>
+auto sum(T a, Args... args) { return a + sum(args...); }
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+// S@ndip : Shree Ganeshay Namah
+// https://codeforces.com/problemset/problem/918/A
+
+
+vector <int> fibonicca = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987};
+
+int main()
+{
+
+    
+    int n;
+    cin >> n;
+
+    vector <int> b;
+    for (int i = 0; i < fibonicca.size(); i++)
+    {
+        if(fibonicca[i] <= n)
+            b.push_back(fibonicca[i]);
+    }
+
+    // for(auto l : b) cout  << l << " ";
+
+    // 
+    string s = "";
+    for (int i = 0; i < n; i++)
+    {
+        s += "o";
+    }
+
+    for (int i = 0; i < b.size(); i++)
+    {
+        s[b[i] - 1] = 'O';
+    }   
+
+    cout << s;
+    
+    return 0;
+}
