1361c80884aa10c7d55e4010fbde7afdcfef7162
Sandip Aravindbhai Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-03-01 07:13:45 +0530

    Add files via upload

---
+++ Daily Dose/260_A_IQ_Test.cpp
@@ -0,0 +1,128 @@
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <numeric>
+#include <iomanip>
+
+using namespace std;
+const int N = 1e7;
+
+using I = int;
+using SI = short int;
+using LI = long int;
+using LLI = long long int;
+using UI = unsigned int;
+using USI = unsigned short int;
+using ULI = unsigned long int;
+using ULLI = unsigned long long int;
+
+#define vi vector<int>
+#define pi pair<int, int>
+#define umap unordered_map
+#define uset unordered_set
+
+#define all(a) a.begin(), a.end() - 1
+#define rall(a) a.rbegin(), a.rend()
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define ws1(a) cout << a << " "
+#define wr1(a) cout << a << endl
+#define wr2(a, b) cout << a << " " << b << endl
+#define wr3(a, b, c) cout << a << " " << b << " " << c << endl
+#define wr4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl
+#define ww1(a) cout << a << "\n"
+#define ww2(a, b) cout << a << " " << b << "\n"
+#define ww3(a, b, c) cout << a << " " << b << " " << c << "\n"
+#define ww4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << "\n"
+#define FOR(i, s, e) for (int i = s; i < e; ++i)
+#define FORR(i, s, e) for (int i = s; i >= e; --i)
+#define FT first
+#define SC second
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define PRE cout << fixed << setprecision(10);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+vector<pair<int, int>> sides = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+vector<pair<int, int>> movements = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+clock_t startTime;
+void getCurrentTime() { cerr << (double)(clock() - startTime) / CLOCKS_PER_SEC; }
+
+int sum() { return 0; }
+template <typename T, typename... Args>
+auto sum(T a, Args... args) { return a + sum(args...); }
+
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+// S@ndip : Shree Ganeshay Namah
+// https://codeforces.com/problemset/problem/328/A
+
+bool check(double n){
+    int k = n;
+
+
+    double r = n - k;
+
+
+    if(r > 0)
+        return false;
+
+
+    return true;
+}
+
+int main()
+{
+
+    vector <int> v(4, 0);
+
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> v[i];
+    }
+
+    set <int> AP;
+    for (int i = 1; i < 4; i++)
+    {
+        AP.insert(v[i] - v[i - 1]);
+    }
+
+    // cout << AP.size() << "\n";
+
+    set <double> GP;
+    for (int i = 1; i < 4; i++)
+    {
+        GP.insert((double)v[i] / v[i - 1]);
+    }
+    
+    // cout << GP.size() << "\n";
+
+
+    if(AP.size() == 1)
+    {
+        cout << v[3] + v[3] - v[2];
+    }
+    else if(GP.size() == 1){
+
+        if(check(v[3] * (v[3] / (double)v[2])))
+            cout << v[3] * (v[3] / (double)v[2]);    
+        else cout << 42;
+    }
+    else{
+        cout << 42;
+    }
+    
+    return 0;
+}
