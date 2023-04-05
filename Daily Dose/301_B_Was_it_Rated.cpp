d9ef42cbb5a1411130830ddd33a36b8fefd070e7
Sandip Aravindbhai Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-04-05 20:41:51 +0530

    April Fool 2023

---
+++ Daily Dose/301_B_Was_it_Rated.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// https://codeforces.com/problemset/problem/1812/B
+
+int main(){
+        
+    int n;
+    cin >> n;
+    
+    
+    if(n == 15 or n == 20 or n == 21)
+        cout << "NO";
+    else cout << "YES";
+    return 0;
+}
