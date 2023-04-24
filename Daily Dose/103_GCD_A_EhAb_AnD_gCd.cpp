42e775d58c388e24eb4cb66d5cd9a2be8ff262b0
Sandip Aravindbhai Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-04-24 05:57:40 +0530

    Add files via upload

---
+++ Daily Dose/103_GCD_A_EhAb_AnD_gCd.cpp
@@ -0,0 +1,28 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_set>
+#include <iomanip>
+using namespace std;
+const int N = 1e5+10;
+
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+// S@ndip : Shree Ganeshay Namah
+// https://codeforces.com/contest/1325/problem/A
+// GCD & LCM Basic
+int main()
+{
+    FIO
+
+    int t;
+    cin >> t;
+    while(t--){
+        int x;
+        cin >> x;        
+        cout << 1 << " " << x - 1 << "\n";
+    }
+    return 0;
+}
