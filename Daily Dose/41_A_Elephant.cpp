4acfa95662e3833c53fce1015e38f0f3d7073514
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-07-06 16:57:56 +0530

    41

---
+++ Daily Dose/41_A_Elephant.cpp
@@ -0,0 +1,18 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+// S@ndip
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n % 5 == 0){
+        cout << n / 5;
+    }
+
+    else{
+        cout << n / 5 + 1;
+    }
+    return 0;
+}
