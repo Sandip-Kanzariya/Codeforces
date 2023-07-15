9fe1734cc00f50778ccc15772b2298355c123cf9
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-07-15 16:24:01 +0530

    game theory

---
+++ Daily Dose/342_A_Subtraction_Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// https://codeforces.com/contest/1844/problem/A
+// S@ndip : Shree Ganeshay Namah
+
+int main()
+{
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b;
+
+        cin >> a >> b;
+
+        cout << a + b << "\n";
+    }
+    return 0;
+}
