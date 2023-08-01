a1892f2d7c228901cc7038259faaa067a30d1536
Sandip Kanzaiya <105594748+Sandip-Kanzariya@users.noreply.github.com>
2023-08-01 15:00:57 +0530

    56

---
+++ Daily Dose/56_A_Again_Twenty_Five.cpp
@@ -0,0 +1,32 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+#define for_loop(i, n) for (int i = 0; i < n; i++)
+#define pb push_back
+#define mp make_pair
+// S@ndip
+int main()
+{
+    long long int n;
+    cin >> n;
+
+    // long long l = 1;
+
+    // for (long long i = 0; i < n; i++)
+    // {
+    //     l *= (long long)(5);
+    // }
+    // cout << l << "\n";
+    // long long last = l % (long long)10;
+    // l = l / (long long)10;
+
+    // cout << l % (long long)10 << last << " ";
+    
+    cout << 25 ;
+    return 0;
+}
